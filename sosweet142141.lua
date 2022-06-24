a86315a2b03fbd9c084dcc5ec7f6b0628577bffe
Soza13234342 <92639844+Soza13234342@users.noreply.github.com>
2022-06-24 19:41:24 +0700

    Update sosweet142141.lua

--- sosweet142141.lua
+++ sosweet142141.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Soza13234342/Soza13234342/main/fwefhqjsdkasdf3.lua'))
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Soza13234342/Soza13234342/main/fwefhqjsdkasdf3.lua'))()
